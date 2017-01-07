76c05ed47834b4fb8753f3eb4244c7f53cbf01dd
jpke <j.patrick.earnest@gmail.com>
2017-01-07 15:49:50 -0500

    thunk needed

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,19 +3,20 @@ import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import { Reducer, initialState } from './reducers.js'
 
-const logger = createLogger()
+// enable logger for debugging store
+// const logger = createLogger()
+// const middleware = [logger, thunk];
 
-const middleware = [logger, thunk];
+const middleware = [thunk];
 
-// enable enhancers for debugging store
-// const enhancers = compose(
-//   applyMiddleware(...middleware),
-//   window.devToolsExtension ? window.devToolsExtension() : f => f
-// )
+const enhancers = compose(
+  applyMiddleware(...middleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
 
 const store = createStore(
   Reducer,
   initialState,
-  // enhancers
+  enhancers
 )
 export default store
